e8bfca52a947790e06e4d6798a691df70e3b6f95
Parewa <waparechutinan@gmail.com>
2023-07-25 14:25:49 +0700

    search-Data-25-7-66

--- src/components/page/Search.jsx
+++ src/components/page/Search.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Layouts from "../Layout";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import axiosEPropertyFolder from '../../api/axios'
 import { Space, Table, Tag } from "antd";
 // import "./insert.css";
 import "../../../src/index.css";
@@ -17,59 +18,76 @@ import useCustomTheme from "../hooks/useCustomTheme";
 import AddIcon from "@mui/icons-material/Add";
 import SearchIcon from "@mui/icons-material/Search";
 
-const columns = [
-  {
-    title: "รหัสแปลงที่ดิน",
-    dataIndex: "name",
-    key: "name",
-    // render: (text) => <a>{text}</a>,
-  },
-
-  {
-    title: "ที่อยู่",
-    dataIndex: "address",
-    key: "address",
-  },
-
-  {
-    title: "",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <Button style={{ color: "#6a5acd" }}>
-          <EditOutlined /> แก้ไขข้อมูล
-        </Button>
-      </Space>
-    ),
-  },
-  {
-    title: "",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <Button style={{ color: "#ff0000" }}>
-          <DeleteOutlined />
-          ลบข้อมูล
-        </Button>
-      </Space>
-    ),
-  },
-];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-];
-
 function Search() {
   const { theme } = useCustomTheme();
   const navigate = useNavigate()
-  const App = () => <Table columns={columns} dataSource={data} />;
-  const ComponentDemo = App;
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [parcelCodeSearch, setParcelCodeSearch] = useState()
+
+  const handleSearchFormSubmit = async (event) => {
+    event.preventDefault()
+    setLoading(true);
+    await axiosEPropertyFolder({
+      method: 'post',
+      url: '/search',
+      data: {
+        parcelCodeSearch: parcelCodeSearch
+      }
+    }).then((response) => {
+      response = response.data
+      if (response.status === 'OK') {
+        setData(response.data);
+      }
+    })
+    setLoading(false);
+  }
+  useEffect(() => {
+    setColumns([
+      {
+        title: "รหัสแปลงที่ดิน",
+        dataIndex: 'รหัสแปลงที่ดิน',
+        width: 400,
+        render: (_, record) => <a>{record.parcel_code}</a>,
+      },
+      {
+        title: "ประเภทเอกสาร",
+        width: 400,
+        render: (_, record) => <a>{record.type}</a>,
+      },
+   
+      {
+        // field: 'Parcel Code',
+        // title: "รหัสแปลงที่ดิน",
+        width: 400,
+        render: (_, record) => (
+          <Space size="middle">
+            <Button style={{ color: "#6a5acd" }}>
+              <EditOutlined /> แก้ไขข้อมูล
+            </Button>
+          </Space>
+        ),
+      },
+      {
+        width: 400,
+        render: (_, record) => (
+          <Space size="middle">
+            <Button style={{ color: "#ff0000" }}>
+              <DeleteOutlined />
+              ลบข้อมูล
+            </Button>
+          </Space>
+        ),
+      }
+    ])
+  }, [])
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+
+
   return (
     <ThemeProvider theme={theme}>
       <div className="otherSide" id="otherSide">
@@ -79,13 +97,20 @@ function Search() {
         <Box className="Box-title">
           <Typography className="text-font">ค้นหาข้อมูล</Typography>
         </Box>
-        <Box className="content">
+        <Box
+          className="content"
+          component="form"
+          onSubmit={handleSearchFormSubmit}
+        >
           <Row
             gutter={[24, 24]}
             style={{ justifyContent: "center", alignItems: "center" }}
+            autoComplete="off"
           >
             <Col>
-              <TextField label="รหัสแปลงที่ดิน" />
+              <TextField
+                onChange={(event) => setParcelCodeSearch(event.target.value)}
+                label="รหัสแปลงที่ดิน" />
             </Col>
             <Col>
               <Button
@@ -107,7 +132,7 @@ function Search() {
                   backgroundColor: "#3cb371",
                 }}
                 variant="contained"
-                type="submit"
+                // type="submit"
                 onClick={() => { navigate('../') }}
               >
 
@@ -119,7 +144,10 @@ function Search() {
         </Box>
         <Box className="content">
           <Row style={{ justifyContent: "center" }}>
-            <ComponentDemo />
+            <Table
+              columns={columns}
+              dataSource={data}
+            />
           </Row>
         </Box>
       </div>
